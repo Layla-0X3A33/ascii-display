830a19f9f85915db5138ec22efa782c1e26db43f
Layla :3 <132763952+Layla-0X3A33@users.noreply.github.com>
2024-04-29 11:37:22 -0400

    added things

--- asciiDisplay.js
+++ asciiDisplay.js
@@ -10,8 +10,8 @@ const displayData = {
 
 function setUpCanvas(width, height, charSize, canvasID) {
 
-    if(document.getElementById(canvasID) !== null &&
-        displayData.canvas.canvasID === undefined){
+    if (document.getElementById(canvasID) !== null &&
+        displayData.canvas.canvasID === undefined) {
 
         displayData.canvas.width = width;
         displayData.canvas.height = height;
@@ -19,15 +19,31 @@ function setUpCanvas(width, height, charSize, canvasID) {
         displayData.canvas.charSize = charSize;
         displayData.canvas.canvasID = canvasID;
         document.getElementById(canvasID).width = (charSize * 10 / 16) * width;
-        document.getElementById(canvasID).height = charSize * height;   
-    
+        document.getElementById(canvasID).height = charSize * height;
+
     }
 
 }
 
 function transferCharData(x1, y1, width1, height1, chars1, x2, y2, width2, height2, chars2) {
     //this function takes 2 arrays of characters with color data that exist in 2d space,
-    //and transfers data from the first to the second where they intersect
+    //and transfers data from the first to the second where they intersect, however it 
+    //does not copy the value 0
 
-    
+    const minX = ((x1 < x2) ? x2 : x1);
+    const minY = ((y1 < y2) ? y2 : y1);
+    const maxX = (x1 + width1 < x2 + width2) ? x1 + width1 : x2 + width2;
+    const maxY = (y1 + height1 < y2 + height2) ? y1 + height1 : y2 + height2;
+
+    for (let i = minX; i < maxX; ++i) {
+        for (let j = minY; j < maxY; ++j) {
+
+            alert(i - x2 + (j - y2) * width2);
+
+            if (chars1[i - x1 + (j - x1) * width1] !== 0)
+                chars2[i - x2 + (j - y2) * width2] =
+                    chars1[i - x2 + (j - y2) * width2];
+
+        }
+    }
 }
