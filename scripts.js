cbd160e8195788ee9f41e3c65e16dbaf979368f5
Layla :3 <132763952+Layla-0X3A33@users.noreply.github.com>
2024-05-03 15:14:25 -0400

    added thing

--- scripts.js
+++ scripts.js
@@ -5,4 +5,12 @@ const display = new AsciiDisplay(
     config.canvas.id
 );
 
+const layer1 = display.addDisplayLayer("layer1");
+const object1 = new AsciiObject(0, 0, 2, 2, [0X3AFFFFFF, 0X33FFFFFF, 0X3AFF0000, 0X33FFFFFF]);
+
+layer1.addAsciiObject = object1;
+
+display.camera.x -= 1;
+display.camera.y -= 1;
+
 display.drawChars();
