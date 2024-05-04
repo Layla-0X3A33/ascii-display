b4faadbfd8ec19afe635e2409c9a027b640786b6
Layla :3 <132763952+Layla-0X3A33@users.noreply.github.com>
2024-05-04 13:42:34 -0400

    made minor changes  to the code and added comments

--- asciiDisplay.js
+++ asciiDisplay.js
@@ -10,48 +10,78 @@ class AsciiDisplay {
 
     constructor(width, height, charSize, canvasID) {
 
-        this.#width = width;
-        this.#height = height;
+        this.#width = width;  //sets width and height.
+        this.#height = height;//these can not be changed after they are set to a value.
+
         this.#chars = new Int32Array(width * height);
-        this.#charSize = charSize;
+        //creates an array that stores display data.
+        //each index is a character with the following format:
+        //most significant byte = ascii value,
+        //next byte = red color value,
+        //next byte = green color value,
+        //next byte = blue color value.
+
+        this.#charSize = charSize;//the size in pixels of each character's height.
+
         document.getElementById(canvasID).width = (charSize * 10 / 16) * width;
+        //the width of the canvas is the 10 16ths of charSize multiplied the width in characters.
+        
         document.getElementById(canvasID).height = charSize * height;
+        //the height of the canvas is the height in characters multiplied by charSize.
+
         this.#ctx = document.getElementById(canvasID).getContext("2d");
+        //gets context of the canvas with the specified ID.
+        
         this.#ctx.font = this.#charSize + "px monospace";
-        this.displayLayers = {};
-        this.#hasShader = false;
-        this.camera = { x: 0, y: 0 };
+        //stes the font to monospace and the size of the font to charSize.
+
+        this.displayLayers = [];
+        //stores the display layers.
+
+        this.#hasShader = false;//if this is false, shaders are
+        //ignored when rendering the ascii.
+
+        this.camera = { x: 0, y: 0 };//this is the camera vector. 
+        //(note: this only effects layers with a relative position)
     }
 
     get width() {
         return this.#width;
-    }
+    }//gets width.
 
     get height() {
         return this.#height;
-    }
+    }//gets height.
 
-    drawChars() {
+    drawChars() {//draws characters to the canvas.
 
         for (let i = this.#width * this.#height; i-- > 0;) {
-            this.#chars[0] = 0;
-        }
+            //I understand that this for loop is cursed.
 
-        for (let layer in this.displayLayers) {
-            let temp = this.displayLayers[layer];
+            this.#chars[i] = 0;
+        }//clears chars of previous values.
+
+        for (let i = this.displayLayers.length; i-- > 0;) {
+
+            if(this.displayLayers[i].display)
             transferCharData(
-                0, 0, this.#width, this.#height, temp.shadedChars,
+                0, 0, this.#width, this.#height, this.displayLayers[i].shadedChars,
                 0, 0, this.#width, this.#height, this.#chars
-            );
+            );//transfers the char data in each layer to this.#chars.
+
         }
 
         if (this.#hasShader) this.#chars = this.#shader(this.#chars);
+        //applies shader if one exists.
+        //(note: the shader function should return an Int32Array of the
+        //length this.#width * this.#heigth)
 
-        for (let i = 0; i < this.#width; ++i) {
-            for (let j = 0; j < this.#height; ++j) {
+        for (let i = this.#width; i-- > 0;) {     //
+            for (let j = this.#height; j-- > 0;) {//loops through each index in chars
 
                 this.#ctx.fillStyle = "#" +
                     (this.#chars[i + j * this.#width] & 0XFFFFFF).toString(16);
+                //sets the fill style to the color data of the index
 
                 this.#ctx.fillText(
                     String.fromCharCode(
@@ -59,31 +89,31 @@ class AsciiDisplay {
                     ),
                     i * (this.#charSize * 10 / 16),
                     j * this.#charSize + (this.#charSize * 14 / 16)
-                );
+                );//draws the character at its position
             }
         }
     }
 
-    addDisplayLayer(layerName) {
+    addDisplayLayer() {
         const layer = new DisplayLayer(this.#width, this.#height, this.camera);
-        this.displayLayers[layerName] = layer;
+        this.displayLayers.push(layer);
         return layer;
-    }
+    }//this adds a layer and returns that layer
 
-    removeDisplayLayer(layerName) {
-        delete this.displayLayers[layerName];
-    }
+    removeDisplayLayer(layer) {
+        this.displayLayers.splice(layer, 1);
+    }//this removes a layer
 
     set shader(shaderFunction) {
         if (typeof shaderFunction === "function") {
             this.#shader = shaderFunction;
         };
         this.#hasShader = true;
-    }
+    }//sets the shader
 
     removeShader() {
-        this.#hasShader = false;
-    }
+        this.#hasShader = false; 
+    }//removes the shader
 
 }
 
@@ -97,15 +127,24 @@ class DisplayLayer {
     #camera;
 
     constructor(width, height, camera) {
-        this.#width = width;
-        this.#height = height;
+        this.#width = width;  //sets the width and heigth to the width and height of the 
+        this.#height = height;//instance of AsciiDisplay that created the layer.
+
         this.#chars = new Int32Array(width * height);
-        this.#shader = undefined;
-        this.#hasShader = false;
-        this.asciiObjects = [];
-        this.display = true;
-        this.#camera = camera;
-        this.relativePosition = true;
+        //creates an array of 32 bit integers to store display data.
+
+        this.#shader = undefined;//
+        this.#hasShader = false; //same as AsciiDisplay, but applied to the layer.
+
+        this.asciiObjects = [];//stores ascii objects in the layer.
+
+        this.display = true;//if this is false, the layer is hidden.
+
+        this.#camera = camera;//this sets the camera to the camera of the AsciiDiaplay that
+        //created the layer.
+
+        this.relativePosition = true;//this controls if objects are displayed from the point of
+        ///the camera or from (0, 0).
     }
 
     set shader(shaderFunction) {
@@ -113,37 +152,42 @@ class DisplayLayer {
             this.#shader = shaderFunction;
         };
         this.#hasShader = true;
-    }
+    }//sets shader
 
     removeShader() {
         this.#hasShader = false;
-    }
+    }//removes shader
 
     set addAsciiObject(asciiObject) {
         this.asciiObjects.push(asciiObject);
-    }
+    }//adds ascii objects to the layer
 
-    get layerAscii() {
+    get layerAscii() {//gets the display data from ascii objects in the layer,
+        //and transfers that data to the layer
 
         for (let i = this.#width * this.#height; i-- > 0;) {
             this.#chars[i] = 0;
-        }
+        }//clears previous data stored in chars
 
         for (let i = 0; i < this.asciiObjects.length; ++i) {
+            //loops through each asciiObject
+
             const temp = this.asciiObjects[i];
+            //stores the current asciiObject as "temp"
+
             transferCharData(
                 temp.x, temp.y, temp.width, temp.height, temp.shadedChars,
                 ((this.relativePosition) ? this.#camera.x : 0),
                 ((this.relativePosition) ? this.#camera.y : 0),
                 this.#width, this.#height, this.#chars
-            );
+            );//transfers the char data from the ascii object to the layer
         }
     }
 
     get shadedChars() {
         this.layerAscii;
         return (this.#hasShader) ? this.#shader(this.#chars) : this.#chars;
-    }
+    }//gets the char data of the layer and if it has a shader, applies a shader to the chars
 }
 
 class AsciiObject {
@@ -151,33 +195,49 @@ class AsciiObject {
     #shader;
     #hasShader;
     #chars;
+    #width;
+    #height;
 
     constructor(x, y, width, height, chars) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        
+        this.x = x;//
+        this.y = y;//x and y positions
+
+        this.#width = width;  //
+        this.#height = height;//width and height
+
         this.#hasShader = false;
+
         if (chars.length === (width * height))
             this.#chars = new Int32Array(chars);
         else
             this.#chars = new Int32Array(width * height);
+        //creates the array that stores chars in the asciiObject,
+        //and if a valid array is provided, it is used.
     }
+
+    get width() {
+        return this.#width;
+    }//gets width
+
+    get height() {
+        return this.#height;
+    }//gets height
 
     set shader(shaderFunction) {
         if (typeof shaderFunction === "function") {
             this.#shader = shaderFunction;
         };
         this.#hasShader = true;
-    }
+    }//sets the shader
 
     removeShader() {
         this.#hasShader = false;
-    }
+    }//removes the shader
 
     get shadedChars() {
         return (this.#hasShader) ? this.#shader(this.#chars) : this.#chars;
-    }
+    }//gets chars and applies a shader if one exists.
 }
 
 function transferCharData(x1, y1, width1, height1, chars1, x2, y2, width2, height2, chars2) {
