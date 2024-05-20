b683965e2f1da387f597ad13feca425a6644b689
Layla :3 <132763952+Layla-0X3A33@users.noreply.github.com>
2024-05-20 17:17:52 -0400

    removed a function

--- asciiDisplay.js
+++ asciiDisplay.js
@@ -470,25 +470,3 @@ function transferCharData(x1, y1, width1, height1, chars1, x2, y2, width2, heigh
         }
     }
 }
-
-function makeBox(w, h) {
-    //returns an array of 32bit integers that make a box of a given size
-    const temp = new Int32Array(w * h);
-  
-    for (let i = w; i-- > 0;) {
-      temp[i] = 0X2DFFFFFF;
-      temp[i + (h - 1) * w] = 0X2DFFFFFF;
-    }
-  
-    for (let i = h; i-- > 0;) {
-      temp[i * w] = 0X7CFFFFFF;
-      temp[w - 1 + i * w] = 0X7CFFFFFF;
-    }
-  
-    temp[0] = 0X2BFFFFFF;
-    temp[w - 1] = 0X2BFFFFFF;
-    temp[w * h - w] = 0X2BFFFFFF;
-    temp[w * h - 1] = 0X2BFFFFFF;
-  
-    return temp;
-}
